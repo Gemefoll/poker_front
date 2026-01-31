39e3b2e65010d34ecd7cb50f9f86d5a716a99b97
Marat <marat-ashirov@list.ru>
2026-01-31 16:05:15 +0300

    added login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, getCurrentInstance } from 'vue'
+import Cookies from 'js-cookie'
+const { appContext } = getCurrentInstance()
+
+const apiUrl = appContext.config.globalProperties.$apiUrl
 
 const username = ref("")
 const password = ref("")
@@ -14,7 +18,7 @@ function validatePassword() {
     return /^[!-~]{6,20}$/.test(password.value)
 }
 
-function sendRegistrationData() {
+async function sendRegistrationData() {
     if (!validateName()) {
         msg.value = "You are dolbaeb, name is bad"
         state.value = 1
@@ -25,13 +29,33 @@ function sendRegistrationData() {
         state.value = 1
         return
     }
-    const user = {
-        Name: username.value,
-        Pass: password.value
+    try {
+        const user = {
+            Name: username.value,
+            Pass: password.value
+        }
+        const res = await fetch(apiUrl + "/user/auth", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "accept": "application/json",
+            },
+            body: JSON.stringify(user)
+        })
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        const data = await res.json()
+        Cookies.set('AccessToken', data.AccessToken, {expires: 30})
+        Cookies.set('RefreshToken', data.RefreshToken, {expires: 30})
+        console.log(Cookies.get('AccessToken'))
+        console.log(Cookies.get('RefreshToken'))
+        msg.value = "Success!"
+        state.value = 2
+    } catch (error) {
+        console.error("Request failed:", error)
+        msg.value = "You are dolbaeb, reason: " + error
     }
-    const userJson = JSON.stringify(user)
-    msg.value = "You are good, " + userJson
-    state.value = 2
 }
 
 </script>
@@ -53,7 +77,7 @@ function sendRegistrationData() {
     <input v-model="password" placeholder="Type you password" />
     <br/>
     <br/>
-    <button @click="sendRegistrationData">Register</button>
+    <button @click="sendRegistrationData">Login</button>
     <h1 :class="{red:state==1, green:state==2}">{{ msg }}</h1>
 
 </template>
